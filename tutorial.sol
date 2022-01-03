4041934e91e776415a2368602f9baf9541713dfc
Orina <oryna.rich26@gmail.com>
2022-01-03 15:14:07 +0300

    commit 2

--- tutorial.sol
+++ tutorial.sol
@@ -1,19 +1,7 @@
+//SPDX-License-Identifier: MIT
 // create a solidity smart contract
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    string value;
-
-    constructor() public {
-        value = "my Value" ;
-    }
-    function get() public view returns (string memory
-    ) {
-        return value;
-
-    }
-
-    function set(string memory  _value) public {
-        value = _value;
-    }
+    string public constant value = "Hello World!";
 }
