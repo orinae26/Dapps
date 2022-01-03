102845b00da2002e0efbe8c9a93d30fa70c706ec
Orina <oryna.rich26@gmail.com>
2022-01-03 15:21:31 +0300

    types in solidity

--- tutorial.sol
+++ tutorial.sol
@@ -3,5 +3,15 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
+
+// types of variables
+
     string public constant value = "Hello World!";
+    bool public  isTrue = true;
+    int public  number = -42;
+    uint public  number2 = 1337;
+    bytes public  mybytes = "Hello World!";
+    bytes32 public  mybytes32 = "Hello World!";
+    uint256 public  myuint256 = 1337;
+    uint8 public  myuint8 = 13;
 }
