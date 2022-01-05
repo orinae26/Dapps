c6b951d739f8c98f78144ae02b8e1a30dfbe29f0
Orina <oryna.rich26@gmail.com>
2022-01-05 14:45:56 +0300

    modifier onlyOwner: onlyOwner can call the function

--- tutorial.sol
+++ tutorial.sol
@@ -6,6 +6,20 @@ contract MyContract {
     uint256 public peopleCount = 0; // number of people in the array
     mapping(uint => Person) public persons; // mapping of uint to Person    
 
+    address public owner; // owner of the contract
+
+    
+    modifier onlyOwner { // modifier to check if the sender is the owner
+       require(msg.sender == owner); // check if the sender is the owner
+        _;
+       
+       //set owner inside a constructor
+    constructor() public {
+        owner = msg.sender;
+       }
+    // only the owner can call this function addPerson
+    }
+
     struct Person { // structs allows you to define custom types
         uint _id; // unique id
         string _name;
@@ -13,10 +27,18 @@ contract MyContract {
         string _email;
     }
 
-    function addperson (string memory _name, uint _age, string memory _email ) public{// allows us instantiate a person
-        peopleCount++; // increment the people count
+    function addperson (string memory _name, uint _age, string memory _email ) public onlyOwner{// allows us instantiate a person
+        incrementCount();
         persons[peopleCount] =  Person(peopleCount, _name, _age, _email);// add the person to the array
          
     } 
+
+    function incrementCount() internal{
+        peopleCount++;
+
+    }
+
+
+
 
 }
